0c0637dc8b2ec5c5bb7c578980b3573ce0dc44e4
pribis <brian.pribis@gmail.com>
2020-11-23 13:47:27 -0500

    Finished import functions
    Finished import so that other developers can use the marc record stream without having to fuss with the other stuff.

--- MarcImport.java
+++ MarcImport.java
@@ -1,43 +1,79 @@
 
 import org.marc4j.MarcReader;
 import org.marc4j.MarcStreamReader;
-import org.marc4j.marc.Record;
 import org.marc4j.marc.DataField;
+import org.marc4j.marc.Subfield;
+import org.marc4j.marc.Record;
+import org.marc4j.marc.VariableField;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
-class MarcImport{
+import java.util.*;
 
-    public int read(String marcFileName){
-        int recCount = 0;
+class MarcImport{
+    InputStream in = null;
+
+    public MarcReader getRecordStream(String marcFileName){
+     
         try{
-            InputStream in = new FileInputStream("import/" + marcFileName); //(currentDir + "/" + marcFileName);
+            in = new FileInputStream("import/" + marcFileName); //(currentDir + "/" + marcFileName);
             MarcReader reader = new MarcStreamReader(in);
-            while (reader.hasNext()) {
-                
-                try{
-                    Record record = reader.next();
-                    DataField title = (DataField) record.getVariableField("245");
-                    System.out.println(title);
-                }catch(Exception e){
-                    System.out.println("There was an error processing this record. Skipping it." + e.getMessage());
-                    continue;
-                }
-                recCount++;
-            }
-            in.close();
+            return reader;
         }catch(FileNotFoundException e){
-            System.out.println(marcFileName + " not found. Files must be put in an import folder under this class file.");
-            return 0;
-        }catch(IOException e){
-            System.out.println("IO Error " + e.getMessage());
-            return 0;
-        }catch(Exception e){
-            System.out.println("WHAT HAPPENED? " + e.getMessage());
+            System.out.println("Could not find the file " + marcFileName);
         }
-        return recCount;
+        return null;
     }
+    public void closeStream(){
+        try{
+            in.close();
+        }catch(IOException e){
+            System.out.println("There was an error closing the stream.");
+        }
+    }
+
+    public HashMap<String, String> getBookData(Record r){
+
+        /*
+            050 = LOC call number
+            100 = author
+            245 = a title c author/editor
+            520 = description
+        */
+        String[] tags = {"050","245", "520"};
+        HashMap<String, String> book = new HashMap<String, String>();
+        List fields = r.getVariableFields(tags);
+        DataField field;
+        for(int idx = 0; idx < fields.size(); idx++){
+            field = (DataField)fields.get(idx);
+            List subFields = field.getSubfields();
+            Iterator i = subFields.iterator();
+            
+            while(i.hasNext()){
+                Subfield subfield = (Subfield)i.next();
+                if(field.getTag().equals("245")){
+                    if(subfield.getCode() == 'a'){
+                        book.put("title", subfield.getData());
+                    }else if(subfield.getCode() == 'c'){
+                        book.put("author", subfield.getData());
+                    }
+                }
+
+                if(field.getTag().equals("520")){
+                    book.put("description", subfield.getData());
+                }
+                if(field.getTag().equals("050")){
+                    book.put("loc_call_number", subfield.getData());
+                }
+                
+        
+            }
+        }
+        return book;
+    }
+
 
 }
--- SimpleLibraryCatalog.java
+++ SimpleLibraryCatalog.java
@@ -1,11 +1,55 @@
+import org.marc4j.MarcReader;
+import org.marc4j.marc.Record;
+
+import java.util.*;
 
 class SimpleLibraryCatalog{
 
     public static void main(String args[]){
 
-        MarcImport mi = new MarcImport();
-        int recCount = mi.read("2213.mrc");
-        System.out.println("Obtained " + Integer.toString(recCount) + " records.");
+       createBookTree("2213.mrc"); 
+       
     }
 
+    /*
+        This is an example function. I've commented what each part does.
+        Just cut and paste what you need. 
+        The important spot is the record stream.
+    */
+    public static void createBookTree(String fileName){
+        int count = 0; //For counting records
+
+        HashMap<String, String> book;  
+           
+        MarcImport mi = new MarcImport();
+        
+        //First we get a record stream
+        MarcReader mr = mi.getRecordStream(fileName);
+        
+        //Now we iterate over the stream, getting a record one at a time.
+        while(mr.hasNext()){
+            Record record = mr.next();
+            /*
+                We take the record of a book.
+                This has more than we need. So we
+                will send it over to get a nicely formatted
+                book as a dictionary.  You can then take each item
+                and add it to your data structure.
+            */
+            book = mi.getBookData(record);
+
+            //I am printing out the available values.
+            //Delete this print statements in your actual function.
+            System.out.println(book.get("title"));
+            System.out.println(book.get("author"));
+            System.out.println(book.get("loc_call_number") + "\n");
+            System.out.println(book.get("description"));
+
+            //Do this if you only want a certain number of records.
+            //Here we will stop at 500.
+            if(count == 500) break;
+            count++;
+        }
+    }
 }
+
